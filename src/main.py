64b5dd13b2daa16cd61c10264906333cacda139e
otomate2002 <otomate2002@gmail.com>
2024-07-02 22:05:21 +0530

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,60 @@
+import logging
+import json
+
+from tempfile import mkdtemp
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def initialise_driver():
+    chrome_options = ChromeOptions()
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-dev-tools")
+    chrome_options.add_argument("--no-zygote")
+    chrome_options.add_argument("--single-process")
+    chrome_options.add_argument(f"--user-data-dir={mkdtemp()}")
+    chrome_options.add_argument(f"--data-path={mkdtemp()}")
+    chrome_options.add_argument(f"--disk-cache-dir={mkdtemp()}")
+    chrome_options.add_argument("--remote-debugging-pipe")
+    chrome_options.add_argument("--verbose")
+    chrome_options.add_argument("--log-path=/tmp")
+    chrome_options.binary_location = "/opt/chrome/chrome-linux64/chrome"
+
+    service = Service(
+        executable_path="/opt/chrome-driver/chromedriver-linux64/chromedriver",
+        service_log_path="/tmp/chromedriver.log"
+    )
+
+    driver = webdriver.Chrome(
+        service=service,
+        options=chrome_options
+    )
+
+    return driver
+
+
+def lambda_handler(event, context):
+    driver = initialise_driver()
+    driver.get("https://wbyte.dev")
+    logger.info(f"Page title: ${driver.title}")
+
+    body = {
+        "title": driver.title
+    }
+
+    response = {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps(body)
+    }
+
+    return response
